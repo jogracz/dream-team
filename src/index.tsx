ba6f9ddf8ddfe3b2d0d95ce9e210f5359217eec2
Joanna Graczyk <joanna.graczyk1@gmail.com>
2021-04-26 19:59:10 +0200

    GTM library

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import TagManager from 'react-gtm-module';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+
+const tagManagerArgs = {
+    gtmId: process.env.REACT_APP_GMT_ID || '',
+}
+
+TagManager.initialize(tagManagerArgs)
 
 ReactDOM.render(
   <React.StrictMode>
